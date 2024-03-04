b06223cb687aed093d9773b80b73aaa9a4ab2d17
Thuannnnnnn <tranquocthuan2003@gmail.com>
2024-03-04 17:11:08 +0700

    commit

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,16 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client"; // Import createRoot
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 import { DarkModeContextProvider } from "./context/darkModeContext";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root")); // Use createRoot
+
+root.render(
   <React.StrictMode>
     <AuthContextProvider>
       <DarkModeContextProvider>
         <App />
       </DarkModeContextProvider>
     </AuthContextProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
